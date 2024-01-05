aa9dcb8eceadc355d1baf557ff61f4ef7f6e091c
Umesh Chandra <umesh.sakinala20@gmail.com>
2024-01-05 06:39:21 +0530

    300.Longest Increasing Subsequence
    Signed-off-by: Umesh Chandra <umesh.sakinala20@gmail.com>

---
+++ 300.Longest-Increasing-Subsequence/300.Longest_Increasing_Subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        for(int i=1; i<nums.size(); i++)
+        {
+            if(temp.back() < nums[i])
+            {
+                temp.push_back(nums[i]);
+            }
+            else
+            {
+                int index = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+                temp[index] = nums[i];
+            }
+        }
+        return temp.size();
+    }
+};
