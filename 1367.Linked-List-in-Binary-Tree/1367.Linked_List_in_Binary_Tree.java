7f8cdf958536aafab04df8ff3e5ef362ad699410
Umesh Chandra <umesh.sakinala20@gmail.com>
2024-09-07 20:51:51 +0530

    1367.Linked List in Binary Tree
    Signed-off-by: Umesh Chandra <umesh.sakinala20@gmail.com>

---
+++ 1367.Linked-List-in-Binary-Tree/1367.Linked_List_in_Binary_Tree.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean check(ListNode head, TreeNode root) {
+        if(head == null) return true;
+        if(root == null || head.val != root.val) return false;
+        return check(head.next, root.left) || check(head.next, root.right);
+    }
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if(root == null) return false;
+        if(check(head, root)) return true;
+        return isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+}
