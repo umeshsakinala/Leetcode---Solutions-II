d5b379bc41e372a2a54345359ab9fd469f9c7244
Umesh Chandra <umesh.sakinala20@gmail.com>
2024-11-06 22:42:27 +0530

    3011.Find if Array Can Be Sorted
    Signed-off-by: Umesh Chandra <umesh.sakinala20@gmail.com>

---
+++ 3011.Find-if-Array-Can-Be-Sorted/3011.Find_if_Array_Can_Be_Sorted.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canSortArray(int[] nums) {
+        int[] values = Arrays.copyOf(nums, nums.length);
+        int n = values.length;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n - i - 1; j++) {
+                if(values[j] <= values[j + 1]) {
+                    continue;
+                }
+                else {
+                    if(Integer.bitCount(values[j]) == Integer.bitCount(values[j + 1])) {
+                        int temp = values[j];
+                        values[j] = values[j + 1];
+                        values[j + 1] = temp;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
